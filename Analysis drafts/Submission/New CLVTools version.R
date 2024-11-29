de889944c921db444c9ea881d834ac4a82b6fb87
HannGuth <guth.hann@gmail.com>
2024-11-29 14:32:13 +0100

    Adapted accessibility for version testing

--- Analysis drafts/Submission/New CLVTools version.R
+++ Analysis drafts/Submission/New CLVTools version.R
@@ -5,12 +5,14 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Load data
 source(paste0(getwd(), "/Data preparation.r"))
-
+# data = list(gift_list = gift_list, el_list = el_list, multi_list = multi_list, apparel_list = apparel_list)
+# saveRDS(data, file = paste0(getwd(), "/VersionTestingData.RData"))
+data = readRDS(paste0(getwd(), "/VersionTestingData.RData"))
 ############ SELECT THE DATA SET HERE
-data_list = gift_list # does not worl for me
-data_list = el_list # works for me 
-data_list = multi_list # does not work for me
-data_list = apparel_list # does not work for me
+data_list = data$gift_list # does not work for me
+data_list = data$el_list # works for me
+data_list = data$multi_list # does not work for me
+data_list = data$pparel_list # does not work for me
 
 set.seed(1)
 # Setting the periods
